173fe59e6b0bf75279f962331aa16cdec621207d
LauraLüll <lauralull20@gmail.com>
2022-02-27 17:27:25 +0200

    Control Flow

---
+++ DiceGame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat - arvuti (cpu) ja kasutaja (user);
+            //mõlemad mängijad viskavad täringuid;
+            //programm kontollib, kumb mängija viskas rohkem;
+            //mängija, kes viskab rohkem, ongi mängu võitja;
+            //*täringuid visatakse kolm korda;
+            //proramm kuulutab võitja.
+
+            Random rnd = new Random();
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                //arvuti vise
+                int cpuRandom = rnd.Next(1, 7);
+                //kasutaja vise
+                int userRandom = rnd.Next(1, 7);
+
+                cpuScore = cpuScore + cpuRandom;
+                userScore = userScore + userRandom;
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}.");
+
+                if (i < 2)
+                {
+                    Console.WriteLine("" + "");
+                    Console.WriteLine("Uuesti viskamiseks vajuta klahvi ''enter'':");
+                    string userName = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("" + "");
+                    Console.WriteLine("Tulemuste kuvamiseks vajuta klahvi ''enter'':");
+                    string userName = Console.ReadLine();
+                }
+            }
+
+            if(cpuScore < userScore)
+            {
+                Console.WriteLine($"Arvuti viskas kokku {cpuScore} punkti. Kasutaja viskas kokku {userScore} punkti.");
+                Console.WriteLine("Kasutaja on mängu võitnud. Palju õnne!");
+            }
+            else if(cpuScore > userScore)
+            {
+                Console.WriteLine($"Arvuti viskas kokku {cpuScore} punkti. Kasutaja viskas kokku {userScore} punkti.");
+                Console.WriteLine("Arvuti on mängu võitnud.");
+            }
+            else
+            {
+                Console.WriteLine($"Arvuti viskas kokku {cpuScore} punkti ja kasutaja visaks samuti {userScore} punkti.");
+                Console.WriteLine("Viik!");
+            }
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus ja salasõna;
+            //kui sisestatud kasutajatunnus on "admin" ja sisestatud salasõna on "admin1234"
+            //siis konsoolis kuvatakse "Tere tulemast!";
+            //muul juhul konsoolis kuvatakse "Vale kasutajatunnus või salasõna. Proovi uuesti.";
+            //kasutajal on kolm katset;
+
+            Console.WriteLine("Sisestage kasutajatunnus ja salasõna. Teil on 3 katset.");
+            Console.WriteLine("" + "");
+
+            int i = 0;
+
+            while (i < 3)
+            {
+                Console.WriteLine("Kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("" + "");
+                Console.WriteLine("Salasõna:");
+                string userPassword = Console.ReadLine();
+                Console.WriteLine("" + "");
+
+                if (userName == "admin" && userPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    i++;
+                    if (i == 1)
+                    {
+                        Console.WriteLine($"Vale kasutajatunnus või salasõna. Proovige uuesti, Teil on {3 - i} katset.");
+                        Console.WriteLine("" + "");
+                    }
+                    else if (i == 2)
+                    {
+                        Console.WriteLine($"Vale kasutajatunnus või salasõna. Proovige uuesti, Teil on veel {3 - i} katse.");
+                        Console.WriteLine("" + "");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Vale kasutajatunnus või salasõna. Kolm katset on kasutatud, proovige 24h pärast uuesti.");
+                    }
+                }
+            }
+        }
+    }
+}
